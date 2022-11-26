d99593b199427b0b2e8b6ad66c4837a2618a1538
Mahdi jafari <mahdijafari3957@gmail.com>
2022-11-26 13:02:59 +0330

    Change for test!

--- lara-app/database/migrations/2022_11_24_135119_create_email_template_table.php
+++ lara-app/database/migrations/2022_11_24_135119_create_email_template_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('body');
-            $table->string('attributes');
+            // $table->string('attributes');
         });
     }
 
