d2390dc82020f03535f77ae5d17c3315a2126c0b
mehdijdev <mahdijafari3957@gmail.com>
2023-02-26 11:49:14 +0330

    The 1 to n Email-EmailTemplate relationship has been established

--- lara-app/tests/Feature/EmailTemplateTest.php
+++ lara-app/tests/Feature/EmailTemplateTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Enums\UserTypeEnum;
+use App\Models\Email;
+use App\Models\EmailTemplate;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,10 +18,10 @@ class EmailTemplateTest extends TestCase
     /** @test for the 1 to n EmailTemplate - Email relation*/
     public function a_emailtemplate_has_many_emails()
     {
-/*         $user = User::factory()->create(['type'=>UserTypeEnum::Applicant]);
-        $request = Request::factory()->create(['applicant_id' => $user->id]);
-        $bid = Bid::factory()->create(['applicant_id'=>$user->id, 'request_id'=>$request->id]);
+        $user = User::factory()->create(['type'=>UserTypeEnum::Applicant]);
+        $emaiTemplate = EmailTemplate::factory()->create();
+        $email = Email::factory()->create(['user_id'=> $user->id, 'template_id'=>$emaiTemplate->id, 'emailable_type' => "App\Models\User", 'emailable_id' => $user->id]);
 
-        $this->assertTrue($request->bids->contains($bid)); */
+        $this->assertTrue($emaiTemplate->emails->contains($email));
     }
 }
