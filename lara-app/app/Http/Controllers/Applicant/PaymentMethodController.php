e8476b46b1ad1ddf71988af1697a94061802844b
mehdijdev <mahdijafari3957@gmail.com>
2023-11-06 20:40:54 +0330

    PAY-191 get requests by filter for guest user has been refactored based on the many-to-many relation changes.

--- lara-app/app/Http/Controllers/Applicant/PaymentMethodController.php
+++ lara-app/app/Http/Controllers/Applicant/PaymentMethodController.php
@@ -13,47 +13,39 @@ use Illuminate\Support\Facades\Log;
 
 class PaymentMethodController extends Controller
 {
-    public function unlinkPaymentMethod($linked_payment_method_id){
+    // public function unlinkPaymentMethod($linked_payment_method_id){
 
-        $applicant = Auth::user();
+    //     $applicant = Auth::user();
 
-        // Check whether the linked method is available for the applicant
-        $linked_method = $applicant->linkedMethods()->where('id',$linked_payment_method_id)->first();
-        if(!$linked_method || $linked_method->status == LinkedMethodStatusEnum::Removed){
-            return response(['message' => 'Linked payment method not found for the applicant.'], 404);
-        }
+    //     // Check whether the linked method is available for the applicant
+    //     $linked_method = $applicant->linkedMethods()->where('id',$linked_payment_method_id)->first();
+    //     if(!$linked_method || $linked_method->status == LinkedMethodStatusEnum::Removed){
+    //         return response(['message' => 'Linked payment method not found for the applicant.'], 404);
+    //     }
 
-        // Check whether the linked method is associated with an active request or bid
-        $valid_to_unlink = true;
+    //     // Check whether the linked method is associated with an active request or bid
+    //     $valid_to_unlink = true;
 
-        $requests_payment_method_ids = $applicant->requests()
-        ->where('status', '!=', RequestStatusEnum::Removed)
-        ->with('paymentMethods:id')
-        ->get()
-        ->pluck('paymentMethods.*.id')
-        ->flatten()
-        ->all();
-        if(in_array($linked_payment_method_id, $requests_payment_method_ids)) {
-            $valid_to_unlink = false;
-        }
-        else {
-            if ($linked_method) {
-                $associatedBids = $linkedMethod->bids()
-                    ->whereNotIn('status', ['rejected', 'invalid'])
-                    ->get();
+    //     $requests_payment_method_ids = $applicant->requests()
+    //     ->where('status', '!=', RequestStatusEnum::Removed)
+    //     ->with('paymentMethods:id')
+    //     ->get()
+    //     ->pluck('paymentMethods.*.id')
+    //     ->flatten()
+    //     ->all();
+    //     if(in_array($linked_payment_method_id, $requests_payment_method_ids)) {
+    //         $valid_to_unlink = false;
+    //     }
+    //     else {
+    //         if ($linked_method) {
+    //             $associatedBids = $linkedMethod->bids()
+    //                 ->whereNotIn('status', ['rejected', 'invalid'])
+    //                 ->get();
 
-        }
-
-
-        Log::info(json_encode($linked_payment_method_id));
+    //     }
 
 
-
-
-
-
-
-
-
-    }
+    //     Log::info(json_encode($linked_payment_method_id));
+    // }
+//}
 }
