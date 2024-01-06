b249f72f8948ab96a291e3ea73bdcc8bd8fa9d73
mehdijdev <mahdijafari3957@gmail.com>
2024-01-06 16:44:04 +0330

    PAY-210 the changes are commited to have a small discussion about the some issues to persue the implementation.

---
+++ lara-app/app/Http/Controllers/Applicant/BidController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Applicant;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AcceptBidRequest;
+use Illuminate\Http\Request;
+
+class BidController extends Controller
+{
+    public function acceptBid(AcceptBidRequest $request){
+
+    }
+}
