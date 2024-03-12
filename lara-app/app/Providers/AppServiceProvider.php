079968b5a4f261e8507b6be4fd557f004c0c2265
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-03-12 09:28:58 +0000

    gitlab ci/cd - active composer

--- lara-app/app/Providers/AppServiceProvider.php
+++ lara-app/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Interfaces\UserRepositoryInterface;
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
