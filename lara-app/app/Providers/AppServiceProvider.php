8b949f30994f03e3802939fcbd5c928bf9ba51cf
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-06-05 10:46:23 +0300

    force https on nightly

--- lara-app/app/Providers/AppServiceProvider.php
+++ lara-app/app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Force https
-        if($this->app->environment('production')) {
+        if($this->app->environment('production') || $this->app->environment('nightly')) {
             URL::forceScheme('https');
         }
 
