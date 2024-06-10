1793df4b8af7bb1ec87e5d7064822dfbad3b1c2a
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-06-10 12:25:06 +0330

    temp

--- lara-app/routes/API/GlobalRoutes.php
+++ lara-app/routes/API/GlobalRoutes.php
@@ -10,4 +10,9 @@ use App\Http\Controllers\API\V1\Public\HealthCheckController;
 
 Route::name('global.')->group(function () {
     Route::get('/health',HealthCheckController::class)->name('health.check');
+    Route::get('/temp',function (){
+        return response()->json([
+            'message' => "Are you kidding me?"
+        ]);
+    })->name('health.temp');
 });
