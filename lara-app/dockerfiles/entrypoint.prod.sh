b41095a6a058869896796581828bea1ee71e50e7
sajjad hadafi <af.hadafi@gmail.com>
2024-06-11 02:40:05 -0400

    feat: test entrypoint

--- lara-app/dockerfiles/entrypoint.prod.sh
+++ lara-app/dockerfiles/entrypoint.prod.sh
@@ -5,6 +5,12 @@ then
     composer install --no-progress --no-interaction
 fi
 
+
+# php artisan storage:link --force
+php artisan optimize:clear
+php artisan optimize
+php artisan migrate --force
+
 COMMAND="php artisan serve --port=$PORT --host=0.0.0.0 --env=.env"
 
 # Execute the appropriate command
