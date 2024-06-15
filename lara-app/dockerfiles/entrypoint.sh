5d652c7fc53a0c4d2752a5d4adb58fa63d8360ba
sajjad hadafi <af.hadafi@gmail.com>
2024-06-15 10:34:28 -0400

    feat: change in local and supervisor

--- lara-app/dockerfiles/entrypoint.sh
+++ lara-app/dockerfiles/entrypoint.sh
@@ -9,24 +9,24 @@ if [[ "$HOSTNAME" == "php" ]]; then
         php artisan passport:install
         php artisan db:seed
     fi
-
-    if [ -z ${!DEPLOY_MODE+x} ]
+    
+    if ["$DEPLOY_MODE" = "false" ]
     then
         # The DEPLOY_MODE env is not set
         if [ ! -f "vendor/autoload.php" ]
         then
             composer install --no-progress --no-interaction
         fi
-
+        
         # php artisan storage:link --force
         php artisan optimize:clear
         php artisan optimize
         php artisan migrate --force
     fi
-
+    
     COMMAND="php artisan serve --port=$PORT --host=0.0.0.0 --env=.env"
-
-elif [[ "$HOSTNAME" == "queue" ]]; then
+    
+    elif [[ "$HOSTNAME" == "queue" ]]; then
     COMMAND="/usr/bin/supervisord -n -c dockerfiles/queues/supervisord.conf"
 else
     echo "[ ENTRYPOINT ] 'HOSTNAME' env is not set"
