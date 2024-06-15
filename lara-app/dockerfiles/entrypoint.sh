4ebfc851242b23b0d1fcf8cb2fc2c3a3ebd80230
sajjad hadafi <af.hadafi@gmail.com>
2024-06-15 06:13:48 -0400

    feat: unify the dockerfile

--- lara-app/dockerfiles/entrypoint.sh
+++ lara-app/dockerfiles/entrypoint.sh
@@ -2,11 +2,6 @@
 
 # Check if the container is running as the PHP or queue service
 if [[ "$HOSTNAME" == "php" ]]; then
-    if [ ! -f "vendor/autoload.php" ]
-    then
-        composer install --no-progress --no-interaction
-    fi
-
     if [ "$RUN_SETUP_COMMANDS" = "true" ]
     then
         # Run the setup commands
@@ -15,17 +10,26 @@ if [[ "$HOSTNAME" == "php" ]]; then
         php artisan db:seed
     fi
 
-    # php artisan storage:link --force
-    php artisan optimize:clear
-    php artisan optimize
-    php artisan migrate --force
+    if [ -z ${!DEPLOY_MODE+x} ]
+    then
+        # The DEPLOY_MODE env is not set
+        if [ ! -f "vendor/autoload.php" ]
+        then
+            composer install --no-progress --no-interaction
+        fi
+
+        # php artisan storage:link --force
+        php artisan optimize:clear
+        php artisan optimize
+        php artisan migrate --force
+    fi
 
     COMMAND="php artisan serve --port=$PORT --host=0.0.0.0 --env=.env"
 
 elif [[ "$HOSTNAME" == "queue" ]]; then
-    COMMAND="php artisan queue:work --tries=3 --backoff=5"
+    COMMAND="/usr/bin/supervisord -n -c dockerfiles/queues/supervisord.conf"
 else
-    echo "Invalid container type"
+    echo "[ ENTRYPOINT ] 'HOSTNAME' env is not set"
     exit 1
 fi
 
