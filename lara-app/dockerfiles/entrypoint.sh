e1debf33ecc3872920ebdb6a2a6b1fc918729b59
sajjad hadafi <af.hadafi@gmail.com>
2024-06-19 09:46:31 -0400

    feat: amend entrypoint

--- lara-app/dockerfiles/entrypoint.sh
+++ lara-app/dockerfiles/entrypoint.sh
@@ -2,25 +2,25 @@
 
 # Check if the container is running as the PHP or queue service
 if [[ "$HOSTNAME" == "php" ]]; then
-    if [ "$RUN_SETUP_COMMANDS" = "true" ]
-    then
+    if [[ "$RUN_SETUP_COMMANDS" == "true" ]]; then
         # Run the setup commands
         php artisan key:generate
         php artisan passport:install
         php artisan db:seed
     fi
     
-    if ["$DEPLOY_MODE" = "false" ]
-    then
-        # The DEPLOY_MODE env is not set
-        if [ ! -f "vendor/autoload.php" ]
-        then
+    if [[ "$DEPLOY_MODE" == "false" ]]; then
+        # The DEPLOY_MODE env is set to false
+        if [[ ! -f "vendor/autoload.php" ]]; then
             composer install --no-progress --no-interaction
         fi
         
+        # Clear and optimize the application cache
         # php artisan storage:link --force
         php artisan optimize:clear
         php artisan optimize
+        
+        # Force database migrations
         php artisan migrate --force
     fi
     
@@ -29,7 +29,7 @@ if [[ "$HOSTNAME" == "php" ]]; then
     elif [[ "$HOSTNAME" == "queue" ]]; then
     COMMAND="/usr/bin/supervisord -n -c dockerfiles/queues/supervisord.conf"
 else
-    echo "[ ENTRYPOINT ] 'HOSTNAME' env is not set"
+    echo "[ ENTRYPOINT ] 'HOSTNAME' env is not recognized"
     exit 1
 fi
 
