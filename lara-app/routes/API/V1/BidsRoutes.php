cac6e2a965e9e1b3a61dcd529a4af6aad15c76e0
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-08-16 10:55:14 +0330

    fix store bid

--- lara-app/routes/API/V1/BidsRoutes.php
+++ lara-app/routes/API/V1/BidsRoutes.php
@@ -10,11 +10,12 @@
 use App\Http\Controllers\API\V1\Bids\User\IndexBidController;
 use App\Http\Controllers\API\V1\Bids\User\StoreBidController;
 use App\Http\Controllers\API\V1\Bids\User\UpdateBidController;
+use App\Models\Bid;
 
 Route::middleware('auth:sanctum')->name('users.bids.')->prefix('users/bids')->group(function () {
     Route::get('/', IndexBidController::class)->name('index');
    // Route::middleware('can:view,request')->get('/{request}', ShowRequestController::class)->name('show');
-    Route::middleware('can:create,bid')->post('/', StoreBidController::class)->name('store');
+    Route::post('/', StoreBidController::class)->name('store');
     Route::middleware('can:update,bid')->patch('/{bid}',UpdateBidController::class)->name('update');
    // Route::middleware('can:delete,paymentMethod')->delete('/{paymentMethod}',DeletePaymentMethodController::class)->name('delete');
 });
