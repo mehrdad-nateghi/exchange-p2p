4dc68ce53166d116e471b5e1e44ffc5014ed800d
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-09-03 15:06:39 +0330

    change pay invoice route to get

--- lara-app/routes/API/V1/InvoiceRoutes.php
+++ lara-app/routes/API/V1/InvoiceRoutes.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\API\V1\Invoice\User\PayInvoiceController;
 Route::middleware('auth:sanctum')->name('users.invoices.')->prefix('users/invoices')->group(function () {
     //Route::get('/', IndexRequestController::class)->name('index');
     //Route::middleware('can:view,request')->get('/{request}', ShowRequestController::class)->name('show');
-    Route::middleware('can:create,invoice')->post('/{invoice}/pay', PayInvoiceController::class)->name('pay');
+    Route::middleware('can:create,invoice')->get('/{invoice}/pay', PayInvoiceController::class)->name('pay');
     //Route::middleware('can:update,request')->put('/{request}',UpdateTradeController::class)->name('update');
     // Route::middleware('can:delete,paymentMethod')->delete('/{paymentMethod}',DeletePaymentMethodController::class)->name('delete');
 });
