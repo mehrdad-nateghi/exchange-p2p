1bca96bf17bc6451f44a1cc266069624d4aa4239
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-09-03 10:54:01 +0330

    show trade

---
+++ lara-app/app/Http/Controllers/API/V1/Trades/User/ShowTradeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Trades\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TradeResource;
+use App\Models\Trade;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+
+class ShowTradeController extends Controller
+{
+    public function __invoke(
+        Trade $trade,
+    ): JsonResponse {
+        try {
+            $resource =  new TradeResource($trade->load('tradeSteps','invoices'));
+
+            return apiResponse()
+                ->message(trans('api-messages.retrieve_success', ['attribute' => trans('api-messages.trade')]))
+                ->data($resource)
+                ->getApiResponse();
+        } catch (\Throwable $t) {
+            Log::error($t);
+            return internalServerError();
+        }
+    }
+}
--- lara-app/routes/API/V1/TradesRoutes.php
+++ lara-app/routes/API/V1/TradesRoutes.php
@@ -12,11 +12,9 @@ use App\Http\Controllers\API\V1\Requests\User\IndexRequestController;
 use App\Http\Controllers\API\V1\Requests\User\ShowRequestController;
 use App\Http\Controllers\API\V1\Requests\User\StoreRequestController;
 use App\Http\Controllers\API\V1\Trades\User\IndexTradeController;
+use App\Http\Controllers\API\V1\Trades\User\ShowTradeController;
 
 Route::middleware('auth:sanctum')->name('users.trades.')->prefix('users/trades')->group(function () {
     Route::get('/', IndexTradeController::class)->name('index');
-    //Route::middleware('can:view,request')->get('/{request}', ShowRequestController::class)->name('show');
-   // Route::post('/', StoreRequestController::class)->name('store');
-   // Route::middleware('can:update,paymentMethod')->put('/{paymentMethod}',UpdatePaymentMethodController::class)->name('update');
-   // Route::middleware('can:delete,paymentMethod')->delete('/{paymentMethod}',DeletePaymentMethodController::class)->name('delete');
+    Route::middleware('can:view,trade')->get('/{trade}', ShowTradeController::class)->name('show');
 });
