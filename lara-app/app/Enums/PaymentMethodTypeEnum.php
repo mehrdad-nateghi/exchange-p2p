7d291e123a6414b4a2c3b360442d3c623535d8bc
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-09-15 10:37:40 +0330

    PAY-1147: implement rial bank logos

--- lara-app/app/Enums/PaymentMethodTypeEnum.php
+++ lara-app/app/Enums/PaymentMethodTypeEnum.php
@@ -10,9 +10,4 @@ enum PaymentMethodTypeEnum: int{
     case RIAL_BANK = 1;
     case FOREIGN_BANK = 2;
     case PAYPAL = 3;
-
-    public function getKeyLowercase(): string
-    {
-        return strtolower($this->name);
-    }
 }
