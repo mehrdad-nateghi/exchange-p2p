c0a4ff20175e8f397f6258af8a89b2ae21a775ed
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-10-20 12:23:56 +0330

    PAY-1069: check response time

--- lara-app/app/Http/Controllers/API/V1/Public/MeTestController.php
+++ lara-app/app/Http/Controllers/API/V1/Public/MeTestController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers\API\V1\Public;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Services\API\V1\UserService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 
 class MeTestController extends Controller
 {
-    public function __invoke(): JsonResponse {
+    public function __invoke(
+        UserService $userService
+    ): JsonResponse {
         try {
+            $user = User::query()->where('id', 10)->first();
             $data = [
-                'user' =>  $userService->createResource(User::query()->where('id',10)->first()),
+                'user' =>  $userService->createResource($user),
             ];
 
             return apiResponse()
