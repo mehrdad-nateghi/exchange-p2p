c9354c944038619a190e995e7ad202e079de6af5
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-10-21 13:34:51 +0330

    PAY-1069: add query log

--- lara-app/app/Providers/AppServiceProvider.php
+++ lara-app/app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Interfaces\UserRepositoryInterface;
 use App\Models\VerificationCode;
 use App\Observers\VerificationCodeObserver;
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
@@ -29,6 +31,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Log a warning if we spend more than 1000ms on a single query.
+        DB::listen(function ($query) {
+            if ($query->time > 200) {
+                Log::warning("An individual database query exceeded 200 ms.", [
+                    'sql' => $query->sql
+                ]);
+            }
+        });
+
         // Force https
         if($this->app->environment('production') || $this->app->environment('nightly')) {
             URL::forceScheme('https');
