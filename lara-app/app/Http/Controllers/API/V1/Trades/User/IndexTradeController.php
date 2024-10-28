72c8196908fc200a62e815b599590a251fa2a86c
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2024-10-28 12:58:03 +0330

    cache user / trades

--- lara-app/app/Http/Controllers/API/V1/Trades/User/IndexTradeController.php
+++ lara-app/app/Http/Controllers/API/V1/Trades/User/IndexTradeController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\TradeCollection;
 use App\QueryFilters\TradeStatusFilter;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -19,33 +20,72 @@ class IndexTradeController extends Controller
     ): JsonResponse
     {
         try {
-            $trades = QueryBuilder::for(Auth::user()->trades())
-                ->with(['tradeSteps' => function($query) {
-                    $query->select('*');
-                }])
-                ->with(['invoices' => function($query) {
-                    $query->select('*');
-                }])
-                ->with(['bid.request', 'bid.paymentMethod']) // Nested eager loading
-                ->select([
-                    'trades.*',
-                ])
-                ->allowedFilters([
-                    AllowedFilter::custom('status', new TradeStatusFilter),
-                ])
-                ->defaultSort(['-created_at'])
-                ->allowedSorts('created_at')
-                ->paginateWithDefault();
+            $cacheKey = 'all_trades_' . $request->fullUrl();
 
-            $trades = new TradeCollection($trades);
+            /*$value = Cache::get('all_trades_' . $request->fullUrl());
+            if (!is_null($value)) {
+                dd($value);
+            }*/
 
-            return apiResponse()
-                ->message(trans('api-messages.retrieve_success', ['attribute' => trans('api-messages.trades')]))
-                ->data($trades)
-                ->getApiResponse();
+            return Cache::remember($cacheKey, 3600, function () use ($request) {
+                $trades = QueryBuilder::for(Auth::user()->trades())
+                    ->with(['tradeSteps' => function($query) {
+                        $query->select('*');
+                    }])
+                    ->with(['invoices' => function($query) {
+                        $query->select('*');
+                    }])
+                    ->with(['bid.request', 'bid.paymentMethod'])
+                    ->select([
+                        'trades.*',
+                    ])
+                    ->allowedFilters([
+                        AllowedFilter::custom('status', new TradeStatusFilter),
+                    ])
+                    ->defaultSort(['-created_at'])
+                    ->allowedSorts('created_at')
+                    ->paginateWithDefault();
+
+                $trades = new TradeCollection($trades);
+
+                return apiResponse()
+                    ->message(trans('api-messages.retrieve_success', ['attribute' => trans('api-messages.trades')]))
+                    ->data($trades)
+                    ->getApiResponse();
+            });
         } catch (\Throwable $t) {
             Log::error($t);
             return internalServerError();
         }
+
+//        try {
+//            $trades = QueryBuilder::for(Auth::user()->trades())
+//                ->with(['tradeSteps' => function($query) {
+//                    $query->select('*');
+//                }])
+//                ->with(['invoices' => function($query) {
+//                    $query->select('*');
+//                }])
+//                ->with(['bid.request', 'bid.paymentMethod']) // Nested eager loading
+//                ->select([
+//                    'trades.*',
+//                ])
+//                ->allowedFilters([
+//                    AllowedFilter::custom('status', new TradeStatusFilter),
+//                ])
+//                ->defaultSort(['-created_at'])
+//                ->allowedSorts('created_at')
+//                ->paginateWithDefault();
+//
+//            $trades = new TradeCollection($trades);
+//
+//            return apiResponse()
+//                ->message(trans('api-messages.retrieve_success', ['attribute' => trans('api-messages.trades')]))
+//                ->data($trades)
+//                ->getApiResponse();
+//        } catch (\Throwable $t) {
+//            Log::error($t);
+//            return internalServerError();
+//        }
     }
 }
