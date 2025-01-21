868650dd3c485e8c0967c65fc50514861b5e13ee
Mehrdad Nateghi <mehrdad.nateghe@gmail.com>
2025-01-21 10:34:18 +0330

    PAY-1292: endpoint: change password

--- lara-app/app/Http/Controllers/API/V1/Users/User/ChangePasswordController.php
+++ lara-app/app/Http/Controllers/API/V1/Users/User/ChangePasswordController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers\API\V1\Users\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\User\User\ChangePasswordRequest;
+use App\Services\API\V1\UserService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 class ChangePasswordController extends Controller
 {
     public function __invoke(
-        ChangePasswordRequest $request
+        ChangePasswordRequest $request,
+        UserService $userService,
     ): JsonResponse {
         try {
-            auth()->user()->update([
+            $user = Auth::user();
+
+            $user->update([
                 'password' => Hash::make($request->new_password)
             ]);
+
+            $userService->logout($request,$user);
 
             return apiResponse()
                 ->message(trans('api-messages.update_success', ['attribute' => trans('api-messages.user')]))
